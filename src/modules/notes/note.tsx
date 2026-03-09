6293903767ecc283bda5eee516c42e2729268e35
Naveen Quodras <naveen.quodras@sunnybrook.ca>
2026-03-09 18:15:53 -0400

    Commit changes from silver laptop.

--- src/modules/notes/note.tsx
+++ src/modules/notes/note.tsx
@@ -1,40 +1,49 @@
+/// <reference path="../../svg.d.ts" />
 import React from "react";
 import { useState } from "react";
 import NoteEditor from "./noteEditor/noteEditor";
+import editDoc from "../../assets/editDoc.svg";
 
 export interface INote {
     id: number;
     title: string;
     details : string
 }
+
 interface NoteProps {
     note : INote
     handleNoteClick : (note: INote) => void
     onNoteSaved : () => void
 }
 export function Note({ note, handleNoteClick, onNoteSaved }:  NoteProps ) {
-    const [isEditorOpen, setEditorOpen] = useState(false);
-
-    const onEditNote = ()=> {
-        setEditorOpen(true);
+    const [isEditingNote, setIsEditingNote] = useState(false);
+    const handleClick = () => {
+        handleNoteClick(note);
     };
-
-    const handleEditorClose = ()=> {
-        setEditorOpen(false);
+    const editNote = () => {
+        setIsEditingNote(true);
     };
-   
     return (
         <>
-        <div className="note" onClick={()=> handleNoteClick(note)}>
-            <h5> {note.id}. {note.title}</h5>
+        <div className="note" onClick={handleClick}>
+            <img src={editDoc} className="icon" />
+            <div className="note-header">
+                <h5> {note.title}</h5>
+                <button onClick={editNote}>Edit</button>
+            </div>
             <div>{note.details}</div>
         </div>
-        <button onClick={onEditNote}>Edit</button>
-        <NoteEditor note={note}
-                    mode="edit"
-                    open={isEditorOpen} 
-                    onClose={handleEditorClose}
-                    onNoteSaved={onNoteSaved} />
+        {isEditingNote && (
+            <NoteEditor
+                note={note}
+                mode="edit"
+                open={isEditingNote}
+                onClose={() => setIsEditingNote(false)}
+                onNoteSaved={onNoteSaved}
+            />
+        )}
         </>
+        
     );
 }
+ 
