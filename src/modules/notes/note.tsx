c96dc43ea892cd7c4e1a6c2aa188b077627186de
Naveen Quodras <naveen.quodras@sunnybrook.ca>
2026-03-12 16:46:35 -0400

    Add DELETE note functionality.

--- src/modules/notes/note.tsx
+++ src/modules/notes/note.tsx
@@ -2,8 +2,10 @@
 import React from "react";
 import { useState } from "react";
 import NoteEditor from "./noteEditor/noteEditor";
-import editDoc from "../../assets/editDoc.svg";
+import editIcon from "../../assets/edit.svg";
+import deleteIcon from "../../assets/delete.svg";
 import './note.css';
+import { API_SERVER } from "../../settings";
 
 export interface INote {
     id: number;
@@ -24,14 +26,30 @@ export function Note({ note, handleNoteClick, onNoteSaved }:  NoteProps ) {
     const editNote = () => {
         setIsEditingNote(true);
     };
+    const deleteNote = () => {
+        fetch(`${API_SERVER}/api/notes/${note.id}`, {
+            method: 'DELETE',
+        })
+        .then(response => response.json())
+        .then(data => {
+            onNoteSaved();
+        })
+        .catch(error => {
+            console.error(error);   
+        });
+    }
     return (
         <>
         <div className="note" onClick={handleClick}>
             <div className="note-header">
-                <img src={editDoc} className="icon--edit-note" onClick={editNote}/>
-                <h5> <span> {note.title}</span></h5>
+                <h5>{note.title}</h5>
+                <span className="note-header-icons">
+                    <img src={editIcon} className="icon--edit-note" onClick={editNote}/>
+                    <img src={deleteIcon} className="icon--delete-note" onClick={deleteNote}/>
+                </span>
             </div>
-            <div>{note.details}</div>
+            
+            <p>{note.details}</p>
         </div>
         {isEditingNote && (
             <NoteEditor
